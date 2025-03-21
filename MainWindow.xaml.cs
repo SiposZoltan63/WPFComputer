83c401ce4c861d3a8277af1d6a4fd9cea7d5a475
SiposZoltan63 <siposz@kkszki.hu>
2025-03-21 13:14:09 +0100

    connect és kinézet kb kész

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,49 @@ namespace feladat0321
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private Connect conn = new Connect();
+        public static int userId = 0;
+        private bool beleptet(string FirstName, string LastName, string pass)
+        {
+            try
+            {
+                conn.Connection.Open();
+
+                string sql = $"SELECT `Id` FROM `data` WHERE `FirstName` = '{FirstName}' and `LastName` = '{LastName}' AND `Password` = '{pass}'";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                bool van = dr.Read();
+
+                conn.Connection.Close();
+                return van;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private void update(string FirstName, string LastName, string Password)
+        {
+            try
+            {
+                conn.Connection.Open();
+
+                string sql = $"UPDATE 'data' SET `FirstName`= '{FirstName}',`LastName`='{LastName}',`Password`='{Password}' WHERE `Id`= '{id}' ;";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+
+                var result = cmd.ExecuteNonQuery();
+
+                conn.Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
